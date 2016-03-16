413c6a903a63f335062cce805c63ea2fddc3d819
darkskeeper <dartskeeper@mail.ru>
2016-03-16 17:26:41 +0300

    some fixes

--- main.cpp
+++ main.cpp
@@ -1,41 +1,26 @@
 #include <QCoreApplication>
 #include <iostream>
-#include <iomanip>
 #include <vector>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int N, num_loses=0, num_wins=0, num_ties=0;
+    int N, num_combs=0, num_win_and_lost;
     cin >> N;
+    num_combs = N * (N-1);
+    num_win_and_lost = num_combs;
     vector <int> cards(N, 0);
+    vector <int> repeats(N+1, 0);
     for ( int i = 0 ; i < N; i++ )
     {
         cin >> cards[i];
+        repeats[cards[i]]++;
     }
-    for ( int i = 0 ; i < N; i++ )
+    for ( int i = 1 ; i <= N; i++)
     {
-        int first_card = cards[i];
-        for ( int k = 0 ; k < N ; k++ )
-        {
-            if(k!=i)
-            {
-                if( first_card < cards[k] )
-                {
-                    num_loses++;
-                }
-                else if( first_card > cards[k] )
-                {
-                    num_wins++;
-                }
-                else
-                {
-                    num_ties++;
-                }
-            }
-        }
+        num_win_and_lost -= repeats[i] * (repeats[i] - 1);
     }
-    cout << (double(num_wins)/(num_wins+num_loses+num_ties)) << " " << (double(num_loses)/(num_wins+num_loses+num_ties));
+    cout << double(num_win_and_lost)/(num_combs*2) << " " << double(num_win_and_lost)/(num_combs*2);
     return a.exec();
 }
