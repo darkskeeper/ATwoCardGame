585aace4ab3fa822b05b514aec95baa0baae4945
darkskeeper <dartskeeper@mail.ru>
2016-03-16 18:26:44 +0300

    some fixes

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int N, num_combs=0, num_win_and_lost;
+    long long N, num_combs=0, num_win_and_lost;
     cin >> N;
     num_combs = N * (N-1);
     num_win_and_lost = num_combs;
-    vector <int> cards(N, 0);
-    vector <int> repeats(N+1, 0);
-    for ( int i = 0 ; i < N; i++ )
+    vector <long long> cards(N, 0);
+    vector <long long> repeats( pow ( 10. , 6) + 1 , 0);
+    for ( long long i = 0 ; i < N; i++ )
     {
         cin >> cards[i];
         repeats[cards[i]]++;
     }
-    for ( int i = 1 ; i <= N; i++)
+    for ( long long i = 1 ; i <= N; i++)
     {
         num_win_and_lost -= repeats[i] * (repeats[i] - 1);
     }
